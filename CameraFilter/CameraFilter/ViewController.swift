6da3a1584203be33da381a9432590930a08dc8fa
Toheed Jahan Khan <>
2023-01-31 13:58:31 +0530

    CameraFilter app - Feature to filter images using RxSwift

--- CameraFilter/CameraFilter/ViewController.swift
+++ CameraFilter/CameraFilter/ViewController.swift
@@ -21,7 +21,38 @@ class ViewController: UIViewController {
         self.navigationController?.navigationBar.prefersLargeTitles = true
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        guard let navController = segue.destination as? UINavigationController, let photosCVC = navController.viewControllers.first as? PhotosCollectionViewController else {
+            fatalError("Segue destination id not found")
+        }
+        
+        photosCVC.selectedPhoto.subscribe(onNext: { [weak self] photo in
+            
+            DispatchQueue.main.async {
+                self?.updateUI(with: photo)
+            }
+        }).disposed(by: disposeBag)
+       
+        
+    }
+    
+    private func updateUI(with image: UIImage) {
+        self.photoImageView.image = image
+        self.applyFilterButton.isHidden = false
+    }
+    
     @IBAction func applyFilterButtonPressed() {
+        guard let sourceImage = self.photoImageView.image else {
+            return
+        }
+        
+        FilterService().applyFilter(to: sourceImage).subscribe(onNext: { filteredImage in
+            
+            DispatchQueue.main.async {
+                self.photoImageView.image = filteredImage
+            }
+        }).disposed(by: disposeBag)
     }
 }
 
