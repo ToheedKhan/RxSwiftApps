02164e840132c75da63852517d97e83ec4c7324c
Toheed Jahan Khan <>
2023-02-01 19:02:34 +0530

    Displayed news in UITableView

--- GoodNews/GoodNews/Controller/NewsTableViewController.swift
+++ GoodNews/GoodNews/Controller/NewsTableViewController.swift
@@ -6,20 +6,47 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class NewsTableViewController: UITableViewController {
     
+    let disposeBag = DisposeBag()
+
     private var articles = [Article]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.navigationController?.navigationBar.prefersLargeTitles = true
-//        navigationItem.largeTitleDisplayMode = .automatic
-//            navigationController?.navigationBar.sizeToFit()
+        //        navigationItem.largeTitleDisplayMode = .automatic
+        //            navigationController?.navigationBar.sizeToFit()
         
-//        self.tableView.contentInsetAdjustmentBehavior = .never
-
+        //        self.tableView.contentInsetAdjustmentBehavior = .never
+        
+        populateNews()
+    }
+    
+    private func populateNews() {
+        let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=7cba6346bfe04056b4d19fd3feef6980")!
+        //just - Returns an observable sequence that contains a single element.
+        Observable.just(url)
+            .flatMap { url -> Observable<Data> in
+            let request = URLRequest(url: url)
+            return URLSession.shared.rx.data(request: request)
+        }.map { data -> [Article]? in
+            print(data)
+            return try? JSONDecoder().decode(ArticleList.self, from: data).articles
+        }.subscribe(onNext: { [weak self] articles in
+            
+            if let articles = articles {
+                self?.articles = articles
+                print(articles)
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+            }
+        }).disposed(by: disposeBag)
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -30,7 +57,15 @@ class NewsTableViewController: UITableViewController {
         return articles.count
     }
     
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//
-//    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as? ArticleTableViewCell else {
+            fatalError("ArticleTableViewCell does not exist")
+        }
+        
+        cell.titleLabel.text = self.articles[indexPath.row].title
+        cell.descriptionLabel.text = self.articles[indexPath.row].description
+        
+        return cell
+    }
 }
--- GoodNews/GoodNews/Models/Article.swift
+++ GoodNews/GoodNews/Models/Article.swift
@@ -11,11 +11,14 @@ struct ArticleList: Decodable {
     let articles: [Article]
 }
 
-extension ArticleList {
-    
-}
-
 struct Article: Decodable {
     let title: String
-    let description: String
+    let description: String?
+}
+
+extension ArticleList {
+    static var all: Resource<ArticleList> = {
+        let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=7cba6346bfe04056b4d19fd3feef6980")!
+        return Resource(url: url)
+    }()
 }
